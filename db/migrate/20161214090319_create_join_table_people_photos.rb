9a376757cef17b2315f76dcfeec5b7c878892d05
ambirdsall <ambirdsall@gmail.com>
2016-12-15 22:06:54 -0800

    Get rid of ill-fit join table

--- db/migrate/20161214090319_create_join_table_people_photos.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTablePeoplePhotos < ActiveRecord::Migration
-  def change
-    create_join_table :people, :photos do |t|
-      t.index [:person_id, :photo_id]
-      t.index [:photo_id, :person_id]
-    end
-  end
-end
