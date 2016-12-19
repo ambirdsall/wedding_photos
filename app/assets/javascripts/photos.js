38c3b694b27c517e6700e5d82dded413f35e3db2
ambirdsall <ambirdsall@gmail.com>
2016-12-19 02:02:39 -0800

    Add keystroke navigation to modal UI
    Note that the script is now again imported in a tag. I briefly went to
    inline tags, both to avoid scoping issues with rails' asset pipeline (no
    IIFE, plz) and to make a small, efficient single-file bundle to deploy
    as a static site while building out more features locally (I love you,
    `curl`); but the asset pipeline mess is easily skipped with some
    slightly hacky `script src="#{asset_path}"` chicanery. Although it's not
    the headline change, it's a significant logistical detail.
    
    So, to make keyboard nav work, there needs to be a reasonably efficient
    representation of the order of photos and there needs to be a set of
    handlers for the actual navigation.
    
    **A Reasonably Efficient Representation of the Order of the Photos**
    Photos are effectively stored as doubly-linked list, with neighboring
    photos' ids stored in data attributes. The list is circular: the first
    and last photos are considered neighbors, not dead ends, but that could
    change.
    
    **A Set of Handlers for the Actual Navigation**
    * Right and left arrow traverse the list
    * Escape dismisses modal and scrolls window to location of modal's
      source photo

--- app/assets/javascripts/photos.js
+++ app/assets/javascripts/photos.js
@@ -1,2 +1,102 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+var state = {
+  photoId:     ''
+, prevPhotoId: ''
+, nextPhotoId: ''
+}
+var modal      = document.getElementById('modal')
+var modalImage = document.getElementById('modal-photo')
+var modalClose = document.getElementById('modal-close')
+var photos     = document.getElementsByClassName('js-photo')
+var numberOfPhotos = photos.length
+  , i
+  , currentPhoto
+
+
+
+
+for (i = 0; i < numberOfPhotos; i++) {
+  currentPhoto = photos[i]
+
+  currentPhoto.onclick = displaySelfAsModal
+}
+modalClose.onclick = hideModal
+
+
+
+
+function hideModal () {
+  window.removeEventListener('keyup', handleKeyup)
+  modal.style.display = 'none'
+}
+
+function displaySelfAsModal () {
+  modal.style.display = 'block'
+
+  modalImage.src = this.src
+  modalImage.alt = this.alt
+
+  state.photoId     = this.id
+  state.prevPhotoId = this.dataset.previd
+  state.nextPhotoId = this.dataset.nextid
+
+  window.addEventListener('keyup', handleKeyup)
+}
+
+function handleKeyup (event) {
+  if (event.defaultPrevented) return
+
+  switch (event.key) {
+    case 'ArrowRight':
+      displayNextAsModal()
+      break
+    case 'ArrowLeft':
+      displayPrevAsModal()
+      break
+    case 'Escape':
+      hideModalAndScrollToPhoto()
+      break
+    default:
+      return
+  }
+}
+
+function displayNextAsModal () {
+  var nextImage = document.getElementById(state.nextPhotoId)
+
+  modalImage.src = nextImage.src
+  modalImage.alt = nextImage.alt
+
+  state.photoId     = nextImage.id
+  state.prevPhotoId = nextImage.dataset.previd
+  state.nextPhotoId = nextImage.dataset.nextid
+}
+
+function displayPrevAsModal () {
+  var prevImage = document.getElementById(state.prevPhotoId)
+
+  modalImage.src = prevImage.src
+  modalImage.alt = prevImage.alt
+
+  state.photoId     = prevImage.id
+  state.prevPhotoId = prevImage.dataset.previd
+  state.nextPhotoId = prevImage.dataset.nextid
+}
+
+function hideModalAndScrollToPhoto () {
+  var photo = document.getElementById(state.photoId)
+
+  window.scroll(0, scrollYPositionOf(photo))
+  hideModal()
+}
+
+function scrollYPositionOf(el) {
+  var accumulatedDistance = 0
+
+  if (el.offsetParent) {
+    do {
+      accumulatedDistance += el.offsetTop
+    } while (el = el.offsetParent)
+
+    return accumulatedDistance
+  }
+}
