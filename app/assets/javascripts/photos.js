f327502f5dd08f3d095ff4f00cff7ab73c53a957
ambirdsall <ambirdsall@gmail.com>
2016-12-29 16:31:51 -0800

    Add thumbnail bar js (no submit yet)
    This is sort of a big one.
    
    First, to help manage the added complexity, I did some reorganizing and
    added a whole lot of vim foldmarkers. To wit:
    
    * Now there's a utils/polyfills section
    * All the UI data and manipulations are extracted and scoped to a `UI`
      object and its methods. This is so tidy to work with, I suspect I'll
      do exactly the same thing with `state`.
    
    The new features:
    
    * Clicking the button or hitting the keyboard shortcut now toggles the
      current photo's selectedness
      - Adds its id number to (or removes it from) a list in `state`
      - Displays or removes its thumbnail from the thumbnail bar
      - Toggles content of the selection button
    * `thumbnailBar` displayed when first photo is selected and hidden when
      last photo is deselected

--- app/assets/javascripts/photos.js
+++ app/assets/javascripts/photos.js
@@ -1,24 +1,180 @@
-var state = {
-  photoId:     ''
-, prevPhotoId: ''
-, nextPhotoId: ''
-}
-var modal          = document.getElementById('modal')
-var modalImage     = document.getElementById('modal-photo')
-var modalCloseBtn  = document.getElementById('modal-close')
-var modalNextBtn   = document.getElementById('modal-next')
-var modalPrevBtn   = document.getElementById('modal-previous')
+// {{{ UTILS / POLYFILLS
+// {{{ DOM MANIPULATION
+function removeElement (el) {//{{{
+  el.parentNode.removeChild(el);
+}//}}}
+function scrollYPositionOf (el) {//{{{
+  var accumulatedDistance = 0
+
+  if (el.offsetParent) {
+    do {
+      accumulatedDistance += el.offsetTop
+    } while (el = el.offsetParent)
+
+    return accumulatedDistance
+  }
+}//}}}
+function scrollTo (el) {//{{{
+  window.scroll(0, scrollYPositionOf(el))
+}//}}}
+// }}}
+// {{{ LANGUAGE
+var slice = [].slice
+function curry(fn) {//{{{
+  var arity = fn.length
+
+  return function f1() {
+    var args = slice.call(arguments, 0)
+
+    if (args.length >= arity) {
+      return fn.apply(null, args)
+    } else {
+      return function f2() {
+        var args2 = slice.call(arguments, 0)
+
+        return f1.apply(null, args.concat(args2))
+      }
+    }
+  }
+}//}}}
+//{{{  polyfill Object.keys
+// From https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
+if (!Object.keys) {
+  Object.keys = (function() {
+    'use strict';
+    var hasOwnProperty = Object.prototype.hasOwnProperty,
+        hasDontEnumBug = !({ toString: null }).propertyIsEnumerable('toString'),
+        dontEnums = [
+          'toString',
+          'toLocaleString',
+          'valueOf',
+          'hasOwnProperty',
+          'isPrototypeOf',
+          'propertyIsEnumerable',
+          'constructor'
+        ],
+        dontEnumsLength = dontEnums.length;
+
+    return function(obj) {
+      if (typeof obj !== 'object' && (typeof obj !== 'function' || obj === null)) {
+        throw new TypeError('Object.keys called on non-object');
+      }
+
+      var result = [], prop, i;
+
+      for (prop in obj) {
+        if (hasOwnProperty.call(obj, prop)) {
+          result.push(prop);
+        }
+      }
+
+      if (hasDontEnumBug) {
+        for (i = 0; i < dontEnumsLength; i++) {
+          if (hasOwnProperty.call(obj, dontEnums[i])) {
+            result.push(dontEnums[i]);
+          }
+        }
+      }
+      return result;
+    };
+  }());
+}//}}}
+// }}}
+// }}}
+// {{{ PHOTOS
 var photos         = document.getElementsByClassName('js-photo')
 var numberOfPhotos = photos.length
   , i
   , currentPhoto
-var mc = new Hammer.Manager(modalImage, {
+// }}}
+// {{{ STATE
+var state = {
+  photoId:           ''
+, prevPhotoId:       ''
+, nextPhotoId:       ''
+, thumbnailSrc:      ''
+, _selectedPhotoIds: {}
+}
+
+state.photoList = function photoList () {//{{{
+  return Object.keys(this._selectedPhotoIds)
+}//}}}
+state.photoList.empty = function empty () {//{{{
+  return state.photoList().length === 0
+}//}}}
+state.photoList.doesNotContain = function doesNotContain (id) {//{{{
+  return !( id in state._selectedPhotoIds )
+}//}}}
+state.photoList.addId = function add (id) {//{{{
+  state._selectedPhotoIds[id] = true
+
+  return this
+}//}}}
+state.photoList.removeId = function removeId (id) {//{{{
+  delete state._selectedPhotoIds[id]
+
+  return this
+}//}}}
+// }}}
+// {{{ UI
+var UI = {
+  modal: {//{{{
+    el: document.getElementById('modal')
+  , image:        document.getElementById('modal-photo')
+  , closeBtn:     document.getElementById('modal-close')
+  , nextBtn:      document.getElementById('modal-next')
+  , prevBtn:      document.getElementById('modal-previous')
+  , downloadBtn:  document.getElementById('modal-download')
+  , selectionBtn: document.getElementById('modal-selection')
+  , selectBtnHTML:   '<span class="glyphicon glyphicon-cloud-download m-h-half-em"></span>Select full-res copy for download <span class="keyboard-shortcut">[shift + enter]</span>'
+  , deselectBtnHTML: '<span class="glyphicon glyphicon-ok selection-check m-h-half-em"></span>Undo selection <span class="keyboard-shortcut">[shift + enter]</span>'
+  , hide: function () {//{{{
+      this.el.style.display = 'none'
+    }//}}}
+  , reveal: function () {//{{{
+      this.el.style.display = 'block'
+    }//}}}
+  , setImage: function (img) {//{{{
+    this.image.src = img.src
+    this.image.alt = img.alt
+
+    this.downloadBtn.href = img.src
+    this.setSelectionBtnFor(img)
+    }//}}}
+  , setSelectionBtnFor: function (photo) {//{{{
+      if ( photo.dataset.selected ) {
+        this.selectionBtn.innerHTML = this.deselectBtnHTML
+      } else {
+        this.selectionBtn.innerHTML = this.selectBtnHTML
+      }
+    }//}}}
+  }//}}}
+, thumbnailBar: {//{{{
+    el: document.getElementById('selected-photos')
+  , gallery: {//{{{
+      el: document.getElementById('selected-photos__list')
+    , add: function (el) {//{{{
+        this.el.appendChild(el)
+      }//}}}
+    }//}}}
+  , reveal: function () {//{{{
+      this.el.style.display = 'flex'
+    }//}}}
+  , hide: function () {//{{{
+      this.el.style.display = 'none'
+    }//}}}
+  }//}}}
+, thumbnail: {//{{{
+    overlayHTML: '<span class="selected-thumbnail__remove">&times;</span>'
+  }//}}}
+}
+//}}}
+var mc = new Hammer.Manager(UI.modal.image, {//{{{
   recognizers: [
     [Hammer.Swipe, { direction: Hammer.DIRECTION_HORIZONTAL }]
   ]
-})
-
-
+})//}}}
+// {{{ EVENT BINDING
 
 
 for (i = 0; i < numberOfPhotos; i++) {
@@ -26,49 +182,14 @@ for (i = 0; i < numberOfPhotos; i++) {
 
   currentPhoto.onclick = displaySelfAsModal
 }
-modalCloseBtn.onclick = dismissModal
-modalPrevBtn.onclick = displayPrevAsModal
-modalNextBtn.onclick = displayNextAsModal
+UI.modal.closeBtn.onclick     = dismissModal
+UI.modal.prevBtn.onclick      = displayPrevAsModal
+UI.modal.nextBtn.onclick      = displayNextAsModal
+UI.modal.selectionBtn.onclick = togglePhotoSelectedness
 
 
-
-
-// Navigating to subsequent photos in the modal doesn't update the scroll position of
-// the window underneath the modal: instead, the window underneath stays
-// blithely focused on the same photo the user was looking at when they first
-// pulled up the modal view. Thus, before hiding the modal, we find the
-// position of the final photo loaded into the modal and scroll the window to
-// that position. That way, the user doesn't get "out of sync" with their
-// mental position in the photo list.
-function dismissModal () {
-  var photo = document.getElementById(state.photoId)
-
-  // scroll window to "focused" photo
-  window.scroll(0, scrollYPositionOf(photo))
-
-  // hide modal
-  modal.style.display = 'none'
-
-  // clean up all modal navigation handlers
-  window.removeEventListener('keyup', handleKeyup)
-  mc.off('swiperight', displayNextAsModal)
-  mc.off('swipeleft', displayPrevAsModal)
-}
-
-// The entry point for the modal UI
-function displaySelfAsModal () {
-  setModalImageTo(this)
-
-  // unveil modal
-  modal.style.display = 'block'
-
-  // set up navigation handlers
-  window.addEventListener('keyup', handleKeyup)
-  mc.on('swiperight', displayPrevAsModal)
-  mc.on('swipeleft', displayNextAsModal)
-}
-
-function handleKeyup (event) {
+//}}}
+function handleKeyup (event) {//{{{
   if (event.defaultPrevented) return
 
   switch (event.key) {
@@ -81,40 +202,164 @@ function handleKeyup (event) {
     case 'Escape':
       dismissModal()
       break
+    case 'Enter':
+      if ( event.shiftKey ) downloadPhoto()
+      // if ( event.altKey ) selectPhoto()
+      if ( event.altKey ) togglePhotoSelectedness()
+      break
     default:
       return
   }
-}
+}//}}}
+function togglePhotoSelectedness () {//{{{
+  var photo = document.getElementById(state.photoId)
 
-function displayNextAsModal () {
+  if ( photo.dataset.selected ) {
+    deselectPhoto()
+  } else {
+    selectPhoto()
+  }
+
+  UI.modal.setSelectionBtnFor(photo)
+}//}}}
+function setModalSelectionBtnContentFor () {//{{{
+}//}}}
+function selectPhoto () {//{{{ // unless already done: display thumbnailBar, display thumbnail, add photoId to list
+  // ensure bar is displayed
+  UI.thumbnailBar.reveal()
+
+  // no double selections, please
+  if ( state.photoList.doesNotContain(state.photoId) ) {
+    markPhotoAsSelectedById(state.photoId)
+    addPhotoToThumbnailBar()
+  }
+
+  // ensure photoId is in state.photoList
+  state.photoList.addId(state.photoId)
+}//}}}
+function deselectPhoto (event) {//{{{
+  var thumbnail
+  var id
+
+  if ( event ) {
+    thumbnail = event.currentTarget
+    id        = thumbnail.dataset.photoId
+  } else {
+    id        = state.photoId
+    thumbnail = document.getElementById('t' + id)
+  }
+
+  state.photoList.removeId(id)
+  markPhotoAsUnselectedById(id)
+
+  thumbnail && removeElement(thumbnail)
+
+  if ( state.photoList.empty() ) UI.thumbnailBar.hide()
+}//}}}
+function markPhotoAsSelectedById (id) {//{{{
+  var photo = document.getElementById(id)
+
+  photo.dataset.selected = true
+}//}}}
+function markPhotoAsUnselectedById (id) {//{{{
+  var photo = document.getElementById(id)
+
+  return delete photo.dataset.selected
+}//}}}
+function addPhotoToThumbnailBar () {//{{{
+  // create elements
+  var newThumbnailWrapper = document.createElement('div')
+  var newThumbnailImage = document.createElement('img')
+  var newThumbnailOverlay = document.createElement('div')
+
+  // add content to elements
+  newThumbnailImage.src               = state.thumbnailSrc
+  newThumbnailOverlay.innerHTML       = UI.thumbnail.overlayHTML
+  newThumbnailWrapper.dataset.photoId = state.photoId
+  newThumbnailWrapper.id              = 't' + state.photoId
+  newThumbnailWrapper.onclick         = deselectPhoto
+
+  // style elements
+  newThumbnailWrapper.classList.add('selected-thumbnail')
+  newThumbnailOverlay.classList.add('selected-thumbnail__overlay')
+
+  // nest elements to make thumbnail subtree
+  newThumbnailWrapper.appendChild(newThumbnailImage)
+  newThumbnailWrapper.appendChild(newThumbnailOverlay)
+
+  // add thumbnail subtree to thumbnailGallery
+  UI.thumbnailBar.gallery.add(newThumbnailWrapper)
+}//}}}
+function downloadPhoto () {//{{{
+  modalDownloadBtn.click()
+}//}}}
+function downloadZipSet () {//{{{
+//   var photoId = state.photoId
+
+//   return new Promise(function(resolve, reject) {
+//     // Standard XHR to load an image
+//     var request = new XMLHttpRequest()
+//     request.open('GET', url)
+//     request.responseType = 'blob'
+//     // When the request loads, check whether it was successful
+//     request.onload = function() {
+//       if (request.status === 200) {
+//         // If successful, resolve the promise by passing back the request response
+//         resolve(request.response)
+//       } else {
+//         // If it fails, reject the promise with a error message
+//         reject(Error('Image didn\'t load successfully; error code:' + request.statusText))
+//       }
+//     }
+//     request.onerror = function() {
+//       // Also deal with the case when the entire request fails to begin with
+//       // This is probably a network error, so reject the promise with an appropriate message
+//       reject(Error('There was a network error.'))
+//     }
+//     // Send the request
+//     request.send()
+//   })
+}//}}}
+function dismissModal () {//{{{ // scrolls to last viewed image in gallery, removes handlers, hides modal
+  scrollTo(document.getElementById(state.photoId))
+
+  // hide modal
+  UI.modal.hide()
+
+  // clean up all modal navigation handlers
+  window.removeEventListener('keyup', handleKeyup)
+  mc.off('swiperight', displayNextAsModal)
+  mc.off('swipeleft', displayPrevAsModal)
+}//}}}
+function displaySelfAsModal () {//{{{ // set modal image, register handlers, display modal
+  setModalImageTo(this)
+
+  // unveil modal
+  UI.modal.reveal()
+  // selectPhoto()
+
+  // set up navigation handlers
+  window.addEventListener('keyup', handleKeyup)
+  mc.on('swiperight', displayPrevAsModal)
+  mc.on('swipeleft', displayNextAsModal)
+}//}}}
+function displayNextAsModal () {//{{{
   var nextImage = document.getElementById(state.nextPhotoId)
 
   setModalImageTo(nextImage)
-}
-
-function displayPrevAsModal () {
+}//}}}
+function displayPrevAsModal () {//{{{
   var prevImage = document.getElementById(state.prevPhotoId)
 
   setModalImageTo(prevImage)
-}
+}//}}}
+function setModalImageTo (sourcePhoto) {//{{{
+  UI.modal.setImage(sourcePhoto)
 
-function setModalImageTo (sourcePhoto) {
-  modalImage.src = sourcePhoto.src
-  modalImage.alt = sourcePhoto.alt
+  state.photoId      = sourcePhoto.id
+  state.prevPhotoId  = sourcePhoto.dataset.previd
+  state.nextPhotoId  = sourcePhoto.dataset.nextid
+  state.thumbnailSrc = sourcePhoto.dataset.thumbnail
+}//}}}
 
-  state.photoId     = sourcePhoto.id
-  state.prevPhotoId = sourcePhoto.dataset.previd
-  state.nextPhotoId = sourcePhoto.dataset.nextid
-}
-
-function scrollYPositionOf(el) {
-  var accumulatedDistance = 0
-
-  if (el.offsetParent) {
-    do {
-      accumulatedDistance += el.offsetTop
-    } while (el = el.offsetParent)
-
-    return accumulatedDistance
-  }
-}
+// vim:foldmethod=marker:foldlevel=0
